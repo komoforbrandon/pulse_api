ca18f9c76ee985288e237179ea9f3595f923961c
KBrand <kbrandn25@gmail.com>
2026-08-03 21:48:51 +0100

    feat: add the yaml dependency and implement functionlity in the docs route

--- src/routes/docsRoute.js
+++ src/routes/docsRoute.js
@@ -2,13 +2,15 @@ import { Router } from "express";
 import swaggerUi from "swagger-ui-express";
 import { readFileSync } from "node:fs";
 import { fileURLToPath } from "node:url";
+import YAML from "yaml";
 
 const router = Router();
 
 const specPath = fileURLToPath(
   new URL("../../docs/openapi.yaml", import.meta.url),
 );
-const spec = JSON.parse(readFileSync(specPath, "utf8"));
+
+const spec = YAML.parse(readFileSync(specPath, "utf8"));
 
 const swaggerHandler = swaggerUi.setup(spec);
 
