ec4a9438416a9752066df6d59e59311319987e32
KBrand <kbrandn25@gmail.com>
2026-08-05 10:38:16 +0100

    fix: remove unused 'next' parameter from error handling

--- src/app.js
+++ src/app.js
@@ -51,7 +51,7 @@ export function createApp() {
   app.use("/status", statusRoute);
   app.use("/docs", docsRoute)
 
-  app.use((err, req, res, next) =>{
+  app.use((err, req, res) =>{
     const status = err.status || err.statusCode || 500;
     const message = err.message || 'Internal Serval Error';
 
